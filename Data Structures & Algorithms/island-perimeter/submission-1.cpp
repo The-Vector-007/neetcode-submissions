09c469738479f439f81d8816e63d22c0e1c369b6
Ajay Singh <ajaysinghrr007@gmail.com>
2026-09-01 23:13:37 +0530

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    vector<int> dj = {1, 0, -1, 0};
+    vector<int> di = {0, 1, 0, -1};
+    vector<vector<bool>> visited;
+
+    bool isSafe(int i, int j, int r, int c){
+        if(i < 0 || i > r-1 || j < 0 || j > c-1){
+            return false;
+        }
+
+        return true;
+    }
+
+    int calculatePerimeter(int i, int j, int r, int c, vector<vector<int>>& grid){
+        int ans = (i-1 < 0) + (j - 1 < 0) + (i + 1 == r) + (j + 1 == c);
+        for(int d = 0; d < 4; d++){
+            if(isSafe(i + di[d], j + dj[d], r, c) && grid[i + di[d]][j + dj[d]] == 0){
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+
+    void dfs(int i, int j, int r, int c, int& ans, vector<vector<int>>& grid){
+        visited[i][j] = true;
+        for(int d = 0; d < 4; d++){
+            int new_i = di[d] + i, new_j = dj[d] + j;
+            if(isSafe(new_i, new_j, r, c) && grid[new_i][new_j] && !visited[new_i][new_j]){
+                ans += calculatePerimeter(new_i, new_j, r, c, grid);
+                dfs(new_i, new_j, r, c, ans, grid);
+            }
+        }
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(grid[i][j] == 1)
+                    ans += calculatePerimeter(i, j, n, m, grid);
+        return ans;
+    }
+};
